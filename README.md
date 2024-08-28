974b363feb0aee16673237bcc0c173d9229f1290
chngoshawk <39177096+chngoshawk@users.noreply.github.com>
2024-08-28 17:01:59 +0800

    init code

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # ttf-compresser
 A small node js project used to transform ttf file to small size
 
-#### todo coding
+##### fontFile Dir store the target .ttf file; font.config.json, you should clare the letter you need, example: "abcdefg"
---
+++ index.js
@@ -0,0 +1,40 @@
+const Fontmin = require('fontmin')
+const fs = require('fs')
+const path = require('path')
+const fontConfigJson = require('./font.config.json')
+try {
+    // fontFile
+    const fontFilePath = './fontFile'
+    if(!fs.existsSync(path.join(process.cwd(), fontFilePath))) {
+        throw 'please mkdir "fontFile" and put in the target .ttf file'
+    }
+    const fontFileDir = fs.readdirSync(fontFilePath)
+    // only .ttf file
+    const fonts = fontFileDir.filter((fd) => {
+        let fileType = fd.split('.')
+        fileType = fileType[fileType.length - 1]
+        return fileType == 'ttf'
+    })
+    if (fonts.length < 1) throw 'no available .ttf file'
+
+    const fontmin = new Fontmin()
+        .src(fontFilePath + '/' + fonts[0]) // only transform the first one .ttf file
+        .use(
+            Fontmin.glyph({
+                fontConfigJson,
+            })
+        )
+        .dest('./output') // output File
+
+    fontmin.run(function (err, files) {
+        if (err) {
+            throw err
+        } else {
+            console.log(
+                'transform completed'
+            )
+        }
+    })
+} catch (err) {
+    console.error(err)
+}
